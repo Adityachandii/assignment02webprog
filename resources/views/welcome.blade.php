569e6b5313ee6a317da19cf4fa3fb8c47c2b1828
Adityachandii <76204985+Adityachandii@users.noreply.github.com>
2023-03-14 16:14:25 +0700

    rombak codingan menggunakan layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<body>
-    <div class="header">
-        <h1>Group 5 Profile</h1>
-    </div>
+@extends('layouts.main')
 
+@section('title', 'Home')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+@endsection
+
+@section('content')
     <div class="isiprofile">
 
         <div class="profile1">
@@ -60,5 +55,5 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
+
